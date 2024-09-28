9edde1488eabe1458b7f5fa5ae40b4eb89d4d7b3
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-09-28 21:58:58 +0900

    [add] - ログイン用のUIだけ

---
+++ src/lib/components/auth/Login.svelte
@@ -0,0 +1,27 @@
+<div class="card bg-base-200 shadow-xl">
+    <div class="card-body">
+        <h2 class="card-title">ログイン</h2>
+
+        <div class="flex flex-col gap-2">
+            
+            <p>ユーザー名</p>
+            <input
+                type="text"
+                placeholder="ユーザー名"
+                class="input input-bordered w-full"
+            />
+
+            <p>パスワード</p>
+            <input
+                type="text"
+                placeholder="ユーザー名"
+                class="input input-bordered w-full"
+            />
+
+            <div class="card-actions justify-end mt-3">
+                <button class="btn btn-block btn-primary">ログイン</button>
+            </div>
+        
+        </div>
+    </div>
+</div>
