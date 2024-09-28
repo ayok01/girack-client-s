63fffac4b8469344f6f405e4f5ba8adc864352bf
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-09-29 00:13:10 +0900

    [add] - ログインに成功したらユーザー情報をStoreへ格納、移動するように

--- src/lib/components/auth/Login.svelte
+++ src/lib/components/auth/Login.svelte
@@ -1,9 +1,68 @@
+<script lang="ts">
+    import { socket } from "$lib/socketHandler/socketInit";
+    import { onDestroy } from "svelte";
+    import { goto } from "$app/navigation";
+    import { userStore } from "$lib/store/userInfoStore";
+    import type { IMyUserinfo } from "$lib/type/user";
+    
+    let username = "";
+    let password = "";
+    let result: "success"|"error"|"" = ""; //認証結果受け取り用
+
+    /**
+     * ログインする
+     */
+    const login = () => {
+        console.log("送るデータ->", username, password);
+        socket.emit("authLogin", {
+            username: username,
+            password: password
+        });
+    }
+
+    /**
+     * ログイン結果の受け取り
+     * @param dat
+     */
+    const SOCKETauthLogin = (
+        dat: {
+            result: string,
+            data: {
+                UserInfo: IMyUserinfo,
+                sessionId: string
+            } | null
+        }
+    ) => {
+        console.log("Login :: SOCKETauthLogin : dat->", dat);
+
+        //受け取りデータがnullじゃないなら成功としてStoreへ格納
+        if (dat.data !== null) {
+            userStore.set(
+                dat.data.UserInfo
+            );
+
+            goto("/");
+        } else {
+            //エラーと設定
+            result = "error";
+        }
+    }
+
+    //Socketハンドラ
+    socket.on("RESULT::authLogin", SOCKETauthLogin);
+
+    onDestroy(() => {
+        socket.off("RESULT::authLogin", SOCKETauthLogin);
+    });
+</script>
+
 <div class="card bg-base-200 shadow-xl">
     <div class="card-body">
         <div class="flex flex-col gap-2">
             
             <p>ユーザー名</p>
             <input
+                bind:value={username}
                 type="text"
                 placeholder="ユーザー名"
                 class="input input-bordered w-full"
@@ -11,14 +70,37 @@
 
             <p>パスワード</p>
             <input
-                type="text"
+                bind:value={password}
+                type="password"
                 placeholder="password..."
                 class="input input-bordered w-full"
             />
 
             <div class="card-actions justify-end mt-3">
-                <button class="btn btn-block btn-primary">ログイン</button>
+                <button
+                    on:click={login}
+                    class="btn btn-block btn-primary"
+                    disabled={username==="" || password===""}
+                >ログイン</button>
             </div>
+
+            {#if result === "error"}
+                <div role="alert" class="alert alert-error">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6 shrink-0 stroke-current"
+                    fill="none"
+                    viewBox="0 0 24 24">
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                    </svg>
+                    <span>ログインに失敗しました。</span>
+                </div>
+            {/if}
         
         </div>
     </div>
