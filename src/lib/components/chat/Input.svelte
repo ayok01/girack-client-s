c540bfa76082f5bcbe8fabd712dc4536f32914ae
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-09-29 04:33:01 +0900

    [add] - 入力部分のコンポ土台作成

---
+++ src/lib/components/chat/Input.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { socket } from "$lib/socketHandler/socketInit";
+
+    let message = "";
+
+</script>
+
+<input
+    type="text"
+    bind:value={message}
+    placeholder="Type your message..."
+    class="flex-grow p-2 border rounded-l-lg"
+/>
+<button on:click={} class="p-2 bg-blue-500 text-white rounded-r-lg">Send</button>
