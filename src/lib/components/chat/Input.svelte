30f3c601b3878bba9450098c1d59d60698bd8d2f
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-09-29 04:52:34 +0900

    [add] - メッセージを送信できるように

--- src/lib/components/chat/Input.svelte
+++ src/lib/components/chat/Input.svelte
@@ -1,15 +1,36 @@
 <script lang="ts">
-    import { socket } from "$lib/socketHandler/socketInit";
-    
-    const channelId = path.split('/').pop()?.toString(); //チャンネルId
-    let message = ""; //メッセージ入力用
+  import { socket } from "$lib/socketHandler/socketInit";
+  import { userStore, sessionIdStore } from "$lib/store/userInfoStore";
+  import { get } from "svelte/store";
 
+  const channelId = window.location.pathname.split("/").pop()?.toString(); //チャンネルId
+  let message = ""; //メッセージ入力用
+
+  /**
+   * メッセージを送信する
+   */
+  const sendMessage = () => {
+    console.log("Input :: sendMessage : userId->", channelId);
+    socket.emit("sendMessage", {
+      RequestSender: {
+        userId: get(userStore).userId,
+        sessionId: get(sessionIdStore),
+      },
+      message: {
+        channelId: channelId,
+        content: message,
+        fileId: [],
+      },
+    });
+  };
 </script>
 
 <input
-    type="text"
-    bind:value={message}
-    placeholder="Type your message..."
-    class="flex-grow p-2 border rounded-l-lg"
+  type="text"
+  bind:value={message}
+  placeholder="Type your message..."
+  class="flex-grow p-2 border rounded-l-lg"
 />
-<button on:click={} class="p-2 bg-blue-500 text-white rounded-r-lg">Send</button>
+<button on:click={sendMessage} class="p-2 bg-blue-500 text-white rounded-r-lg"
+  >Send</button
+>
