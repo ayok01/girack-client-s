bda6c3142a5f9eb1245ddae863da0530e895d17d
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-09-29 00:58:34 +0900

    [add] - クッキー認証をできるように

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,8 +1,70 @@
 <script lang="ts">
+    import { socket } from "$lib/socketHandler/socketInit";
+    import { goto } from "$app/navigation";
     import Login from "$lib/components/auth/Login.svelte";
     import Register from "$lib/components/auth/Register.svelte";
+    import { onDestroy, onMount } from "svelte";
+    import { userStore } from "$lib/store/userInfoStore";
+    import { get } from "svelte/store";
 
     let viewMode: "login" | "register" = "login";
+    let cookieAuthData = {
+        userId: "",
+        sessionId: ""
+    };
+
+    /**
+     * クッキーによる認証結果の受け取り
+     * @param dat
+     */
+    const SOCKEtauthSession = (dat:{result:string, data:boolean}) => {
+        console.log("/auth :: dat->", dat);
+        if (dat.data) {
+            //自分用のユーザー情報受け取り
+            socket.emit("fetchUserInfo", {
+                RequestSender: {
+                    userId: cookieAuthData.userId,
+                    sessionId: cookieAuthData.sessionId
+                },
+                userId: cookieAuthData.userId
+            });
+            //トップへ移動
+            goto("/");
+        }
+    }
+
+    //マウント時、クッキーに認証情報があればそれで処理し始める
+    onMount(() => {
+        socket.on("RESULT::authSession", SOCKEtauthSession);
+
+        //クッキーからデータを読み込んでみる
+        const cookieUserId = document.cookie
+            .split("; ")
+            .find((row) => row.startsWith("userId="))
+            ?.split("=")[1];
+        const cookieSessionId = document.cookie
+            .split("; ")
+            .find((row) => row.startsWith("sessionId="))
+            ?.split("=")[1];
+
+        //クッキーからの認証情報がundefinedでないなら認証
+        if (cookieUserId !== undefined && cookieSessionId !== undefined) {
+            //変数に格納
+            cookieAuthData.sessionId = cookieSessionId;
+            cookieAuthData.userId = cookieUserId;
+            //あらかじめユーザーIdを自分情報用のStoreへ登録
+            userStore.set({...get(userStore), userId: cookieAuthData.userId})
+
+            socket.emit("authSession", {
+                userId: cookieUserId,
+                sessionId: cookieSessionId
+            });
+        }
+    });
+
+    onDestroy(() => {
+        socket.off("RESULT::authSession", SOCKEtauthSession);
+    });
 </script>
 
 <div class="md:w-1/2 xl:w-1/3 w-full mx-auto">
