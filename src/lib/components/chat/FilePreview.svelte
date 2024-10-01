32dc2b0d714159d721a568c85e2c2493118f3a73
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-10-01 17:47:52 +0900

    [add] - ファイル情報を取得して画像だったら表示するように

--- src/lib/components/chat/FilePreview.svelte
+++ src/lib/components/chat/FilePreview.svelte
@@ -1,5 +1,66 @@
 <script lang="ts">
+  import { socket } from "$lib/socketHandler/socketInit";
+  import { userStore, sessionIdStore } from "$lib/store/userInfoStore";
+  import { PUBLIC_BACKEND_ADDRESS } from "$env/static/public";
+  import type { IFile } from "$lib/type/file";
+  import { onDestroy, onMount } from "svelte";
+  import { get } from "svelte/store";
+
   export let fileId: string[];
+
+  //ファイル情報格納用
+  let fileDatas: {
+    [key: string]: IFile;
+  } = {};
+
+  $: fileDatas;
+
+  /**
+   * ファイル情報の受け取り
+   * @param dat
+   */
+  const SOCKETfetchFileInfo = (dat: {
+    result: string;
+    data: {
+      fileId: string;
+      fileInfo: IFile | null;
+    };
+  }) => {
+    console.log("FilePreview :: SOCKETfetchFileInfo : dat->", dat);
+    if (dat.data.fileInfo !== null) {
+      //ファイル情報を格納する
+      fileDatas[dat.data.fileId] = dat.data.fileInfo;
+    }
+  };
+
+  onMount(() => {
+    socket.on("RESULT::fetchFileInfo", SOCKETfetchFileInfo);
+
+    //ファイルIdごとに情報取得
+    for (const id of fileId) {
+      socket.emit("fetchFileInfo", {
+        RequestSender: {
+          userId: get(userStore).userId,
+          sessionId: get(sessionIdStore),
+        },
+        fileId: id,
+      });
+    }
+  });
+
+  onDestroy(() => {
+    socket.off("RESULT::fetchFileInfo", SOCKETfetchFileInfo);
+  });
 </script>
 
 <p>ここでファイル表示</p>
+{#each fileId as id}
+  <p>{fileDatas[id]?.name || "loading..."}</p>
+  <img
+    style="max-height:100px; width:auto;"
+    alt={"画像 : " + id}
+    src={fileDatas[id]?.type.startsWith("image/")
+      ? PUBLIC_BACKEND_ADDRESS + "/downloadfile/" + id
+      : null}
+  />
+{/each}
